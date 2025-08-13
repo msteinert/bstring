cd83369b56d9b4cd19ba1e78b4d6e687fae85e83
Daniel Markstedt <daniel@mindani.net>
2025-08-13 12:40:46 +0200

    Refactor logic to have only one control flow statement per branch
    Separate the return and break statements into separate conditionals, to make it easer to parse for humans and code coverage tools

--- bstring/bstraux.c
+++ bstring/bstraux.c
@@ -587,10 +587,10 @@ bUuDecLine(void *parm, int ofs, int len)
 		}
 		outoctet[0] = (unsigned char)((c0 << 2) | ((unsigned)c1 >> 4));
 		if (t->slen+1 >= llen) {
-			if (0 > bconchar(t, (char)outoctet[0])) {
-				return -__LINE__;
-			}
 			break;
+		}
+		if (t->slen+1 >= llen && 0 > bconchar(t, (char)outoctet[0])) {
+			return -__LINE__;
 		}
 		if ((unsigned)c2 >= 0x40) {
 			if (!ret) {
